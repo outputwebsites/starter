6176cac3babbca443b9079b555d0182db65d189c
derek-perry <dp@derek-perry.com>
2024-12-07 22:09:48 -0500

    Add Initial Files
    Added initial kit files ready for testing and configuration.

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+export default defineConfig({
+  site: "https://outputstarter.netlify.app", // update me!
+  integrations: [
+    icon(),
+    sitemap({
+      filter: (page) => !page.includes("/admin"),
+      changefreq: "weekly",
+      priority: 0.7,
+    }),
+  ],
+});
